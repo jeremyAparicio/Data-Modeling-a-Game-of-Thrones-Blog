d1f3343d49709e9cc2d89452383e1ef4984f67db
jeremyAparicio <jeremy.aparicio.g@gmail.com>
2022-12-22 23:03:32 +0000

    Se realizo diagrama en base a BD

--- src/models.py
+++ src/models.py
@@ -8,23 +8,39 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Usuario(Base):
+    __tablename__ = 'usuario'
+    id_usuario = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False)
+    apellido = Column(String(250), nullable=False)
+    correo = Column(String(250), nullable=False)
+    contraseña = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Personajes(Base):
+    __tablename__ = 'personaje'
+    id_personaje = Column(Integer, primary_key=True)
+    nombre = Column(String(250))
+    titulo = Column(String(250))
+    casa= Column(String(250), nullable=False)
+    perso_a_casa = Column(Integer, ForeignKey('casa.id_casa'))
+    perso_a_conti = Column(Integer, ForeignKey('continente.id_continente'))
+    perso_a_fav = Column(Integer, ForeignKey('favorito.id_favoritos'))
+
+class Casas(Base):
+    __tablename__ = 'casa'
+    id_casa = Column(Integer, primary_key=True)
+    nombre = Column(String(250))
+
+class Continentes(Base):
+    __tablename__ = 'continente'
+    id_continente = Column(Integer, primary_key=True)
+    nombre = Column(String(250))
+    conti_a_fav = Column(Integer, ForeignKey('favorito.id_favoritos'))
+
+class Favoritos(Base):
+    __tablename__ = 'favorito'
+    id_favoritos = Column(Integer, primary_key=True)
+    user_a_fav = Column(Integer, ForeignKey('usuario.id_usuario'))
 
     def to_dict(self):
         return {}
